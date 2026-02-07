bc2f25664ed7f9b5b232386be08d0bfbbbfd977a
RaniaTmar <rania.tmar@esprit.tn>
2026-02-07 02:47:08 +0100

    android

---
+++ twa-android/app/src/main/java/app/web/maratech_impact/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.web.maratech_impact.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
